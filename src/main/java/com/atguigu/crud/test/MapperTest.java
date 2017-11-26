5a70ef4c35d1b89f9120e2b646359d10b5b17a57
tbotbotb <403621692@qq.com>
2017-11-27 00:48:34 +0800

    使用SpringMvcJunit测试分页查询职工方法

--- src/main/java/com/atguigu/crud/test/MapperTest.java
+++ src/main/java/com/atguigu/crud/test/MapperTest.java
@@ -46,11 +46,11 @@ public class MapperTest {
 		//2.插入员工数据
 //		employeeMapper.insertSelective(new Employee(null, "Jerry", "M", "Jerry@atguigu.com", 1));
 		//3.批量插入多个员工;使用sqlSession
-		EmployeeMapper mapper=sqlSession.getMapper(EmployeeMapper.class);
-		for (int i = 0; i < 1000; i++) {
-			String uid= UUID.randomUUID().toString().substring(0, 5)+i;
-			mapper.insertSelective(new Employee(null, uid, "M", uid+"@atguigu.com", 1));
-		}
-		System.out.println("批量完成");
+//		EmployeeMapper mapper=sqlSession.getMapper(EmployeeMapper.class);
+//		for (int i = 0; i < 1000; i++) {
+//			String uid= UUID.randomUUID().toString().substring(0, 5)+i;
+//			mapper.insertSelective(new Employee(null, uid, "M", uid+"@atguigu.com", 1));
+//		}
+//		System.out.println("批量完成");
 	}
 }
